52813e82f7eb60f907e66102e6a5c15410a49ab3
besevero <besevero@gmail.com>
2017-06-14 00:17:19 -0300

    Criação ProdutoDAO
    Criada a classe ProdutoDAO com os métodos de consulta por nome e por
    tipo e de atualização de quantidade. A classe arquivos foi excluída. Os
    erros gerados pela exclusão da classe deve indicar os locais de
    persistência.

--- AutoAtendimento_PCS_SGBD/src/DAO/ProdutoDAO.java
+++ AutoAtendimento_PCS_SGBD/src/DAO/ProdutoDAO.java
@@ -1,7 +1,6 @@
 package DAO;
 
 import Model.Produto;
-import DAO.Infra;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,7 +15,10 @@ public class ProdutoDAO {
     
     private Infra con = new Infra();
     
-    public List consultarProduto(String nome) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException{
+    /*
+    * Pesquisa no banco de dados e retorna os itens com determinado nome
+    */
+    public List consultarProdutoNome(String nome) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException{
         PreparedStatement stmt = null;
         ResultSet rs = null;
         List listaProduto = new ArrayList<>();
@@ -45,58 +47,54 @@ public class ProdutoDAO {
         }catch(SQLException e){}
         return listaProduto;
     }
-    public void inserirAutor(Autor autor){
+    /*
+    * Pesquisa no banco de dados e retorna os itens com determinado tipo
+    */
+      public List consultarProdutoTipo(String tipo) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException{
         PreparedStatement stmt = null;
         ResultSet rs = null;
-                
+        List listaProduto = new ArrayList<>();
+        Produto produto = null;
+        
         try{
-            abrirConexao();
+            con.abrirConexao();
   
-            String sql = "INSERT INTO autores(nome, nome_de_citacao, cpf)VALUES(?,?,?);";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1,autor.getNome());
-            stmt.setString(2,autor.getNome_de_citacao());
-            stmt.setInt(3,autor.getCpf());
+            String sql = "SELECT * FROM produto WHERE tipo = ?;";
+            stmt = con.getConn().prepareStatement(sql);
+            stmt.setString(1,tipo);
             rs = stmt.executeQuery();
             
+            while(rs.next()){
+                produto = new Produto();
+                produto.setNome(rs.getString("nome"));
+                produto.setPreco(rs.getInt("preco"));
+                produto.setQuantidade(rs.getInt("quantidade"));
+                produto.setTipo(rs.getString("tipo"));
+                listaProduto.add(produto);
+            }
             rs.close();
             stmt.close();
-            
-            fecharConexao();
+            con.fecharConexao();
             
         }catch(SQLException e){}
+        return listaProduto;
     }
-    public void atualizarAutor(Autor autor, String nomeAlterar){
+    /*
+      *Atualiza a quantidade de itens disponível de determinado produto
+      */
+   public void atualizarQuantidadeProduto(String nomeAlterar){
         PreparedStatement stmt = null;
         ResultSet rs = null;
                 
         try{
-            abrirConexao();
-            String sql = "UPDATE autores SET nome = ?, nome_de_citacao = ?, cpf = ? WHERE nome = ?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1,autor.getNome());
-            stmt.setString(2,autor.getNome_de_citacao());
-            stmt.setInt(3,autor.getCpf());
-            stmt.setString(4,nomeAlterar);
+            con.abrirConexao();
+            String sql = "UPDATE produto SET quantidade = 50 WHERE nome = ?";
+            stmt = con.getConn().prepareStatement(sql);
+            stmt.setString(1, nomeAlterar);
             rs = stmt.executeQuery();
             rs.close();
             stmt.close();
-            fecharConexao();
-        }catch(SQLException e){}
-    }
-    public void excluirAutor(String nome){
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
-                
-        try{
-            abrirConexao();
-            String sql = "DELETE FROM autores WHERE nome = ?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1,nome);
-            rs = stmt.executeQuery();
-            rs.close();
-            stmt.close();
-            fecharConexao();
+            con.fecharConexao();
         }catch(SQLException e){}
     }
 
