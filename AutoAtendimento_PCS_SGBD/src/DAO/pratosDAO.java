df09b381977565c500728169c3379e19ba2d849c
besevero <besevero@gmail.com>
2017-06-19 20:17:51 -0300

    correções na unirio

--- AutoAtendimento_PCS_SGBD/src/DAO/pratosDAO.java
+++ AutoAtendimento_PCS_SGBD/src/DAO/pratosDAO.java
@@ -21,36 +21,45 @@ public class pratosDAO {
     public pratosDAO(){};
     public void insert_prato(Pedido p){
         PreparedStatement stmt_componente = null;
-        PreparedStatement stmt_pedido = null;
-        
+            
         ResultSet rs = null;
         
         try{
           infra.abrirConexao();
           
-            String sql_componente = "INSERT INTO componentes(senha_pedido, nome_componente) VALUES(?, ?);";
+            String sql_componente = "INSERT INTO componentes(senha_componente, nome_componente) VALUES(?, ?);";
+            
                         
             for(int i = 0; i<=p.getPratos().size();i++){
-                for(int j = 0; i<=p.getPratos().size();j++){
+                
                     stmt_componente = infra.getConn().prepareStatement(sql_componente);
-                    stmt_componente.setString(1,p.getPratos().get(j).getNome());
-                    stmt_componente.setInt(2,p.getSenha());
+                    stmt_componente.setInt(1,p.getSenha());
+                    stmt_componente.setString(2,p.getPratos().get(i).getNome());
                     rs = stmt_componente.executeQuery();
-                }
             }
-            
-            String sql_pedido = "INSERT INTO pedido(senha_pedido, preco) VALUES(?, ?);";
-            stmt_pedido = infra.getConn().prepareStatement(sql_pedido);
-            stmt_pedido.setInt(1,p.getSenha());
-            stmt_pedido.setFloat(1, (float)p.getPreco());
-            rs = stmt_pedido.executeQuery();
+                        
+            insert_pedido(p);
                 
             rs.close();
             stmt_componente.close();
-            stmt_pedido.close();
+            
             infra.fecharConexao();
             
         }catch(SQLException e){}
                
     }
+     public void insert_pedido(Pedido p) throws SQLException{
+          PreparedStatement stmt_pedido = null;
+          ResultSet rs = null;
+          String sql_pedido = "INSERT INTO pedido(senha_pedido, preco) VALUES(?, ?);";
+          try{
+            stmt_pedido = infra.getConn().prepareStatement(sql_pedido);
+            stmt_pedido.setInt(1, p.getSenha());
+            stmt_pedido.setDouble(2, p.getPreco());
+            rs = stmt_pedido.executeQuery();
+          }
+          catch(SQLException e){}
+          rs.close();
+          stmt_pedido.close();
+     }
 }
